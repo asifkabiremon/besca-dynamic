ad8c6b7ef5b67ec7afffbf726a4d481ea683137e
Md Asif Kabir Emon <asif.kabir@coolriots.io>
2023-10-02 15:56:57 +0800

    Update uri

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 
 const templateRoute = require('./src/routes/template.route.jsx');
 const getDataRoute = require('./src/routes/getData.route.jsx');
