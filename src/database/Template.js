ed4d3b0bcf3fb625ddef625ec0be65149807ef57
Md Asif Kabir Emon <asif.kabir@coolriots.io>
2023-10-02 16:24:29 +0800

    Update schema

--- src/database/Template.js
+++ src/database/Template.js
@@ -1,22 +1,10 @@
 const mongoose = require("mongoose");
 
 const templateSchema = new mongoose.Schema({
-  templateName: {
-    type: String,
-    required: true,
-  },
-  searchComponent: {
-    type: Array,
-    required: true,
-  },
-  tableComponent: {
-    type: Array,
-    required: true,
-  },
-  tableStyle: {
-    type: Array,
-    required: true,
-  }
+  templateName: String,
+  searchComponent: Array,
+  tableComponent: Array,
+  tableStyle: Object,
 });
 
 const templateCollection = mongoose.model("templates", templateSchema);
