16d2e2416bc639d1886f6684c1bb406b7462c133
Md Asif Kabir Emon <asif.kabir@coolriots.io>
2023-10-05 10:10:21 +0800

    get countries by search

--- src/routes/getCountry.route.jsx
+++ src/routes/getCountry.route.jsx
@@ -26,4 +26,55 @@ router.get("/country", async (req, res) => {
   }
 });
 
+// This is the route for getting all the cities of a country
+router.get("/city/:country", async (req, res) => {
+  try {
+    const country = req.params.country;
+    const filePath = await path.join(
+      __dirname,
+      `../../public/country.json`
+    );
+    // console.log(filePath);
+    const file = await fs.readFile(filePath, "utf8");
+    const data = JSON.parse(file);
+    // console.log(data);
+    if (!data[country]) {
+      res.status(404).send("");
+      return;
+    }
+    const cities = data[country];
+
+    res.send(cities);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// This is the route for getting a country with partial name
+router.get("/country/:country", async (req, res) => {
+  try {
+    const country = req.params.country;
+    console.log(country);
+    const filePath = await path.join(
+      __dirname,
+      `../../public/country.json`
+    );
+    // console.log(filePath);
+    const file = await fs.readFile(filePath, "utf8");
+    const data = JSON.parse(file);
+    // console.log(data);
+    
+    
+    // search country with partial name
+    const countries = Object.keys(data);
+    const result = countries.filter((c) => c.startsWith(country));
+    res.send(result);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
 module.exports = router;
