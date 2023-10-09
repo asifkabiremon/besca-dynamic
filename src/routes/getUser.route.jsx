88ed748c225825f3b8eab239355ba5277a9e6942
Asif Kabir Emon <asif.kabir@coolriots.io>
2023-10-09 09:57:52 +0800

    add id

--- src/routes/getUser.route.jsx
+++ src/routes/getUser.route.jsx
@@ -6,7 +6,7 @@ const path = require("path");
 
 const router = express.Router();
 
-// This is the route for getting the data
+// This is the route for getting data
 router.post("/", async (req, res) => {
   const target = req.body.target;
   const action = req.body.action;
@@ -52,7 +52,6 @@ router.post("/", async (req, res) => {
       // get the data for the filtered columns
       const filteredDataDisplay = filteredData.map((user) => {
         const filteredUser = {};
-        user.id && (filteredUser.id = user.id);
         tableCol.forEach((column) => {
           filteredUser[column] = user[column];
         });
@@ -74,67 +73,3 @@ router.post("/", async (req, res) => {
     res.status(500).send(error);
   }
 });
-
-// This is the route for getting the data from the database
-// router.post("/db", async (req, res) => {
-//   const target = req.body.target;
-//   const action = req.body.action;
-//   // console.log(req.body);
-//   try {
-//     if (action === "search") {
-//       const parameters = req.body.parameters;
-//       const display = req.body.display;
-//       const maxRow = req.body.maxRow;
-//       const page = req.body.page;
-
-//       const tableCol = Object.keys(display);
-
-//       const selectedField = {"_id": 0};
-//       tableCol.map((column) => selectedField[column] = 1);
-//       console.log(selectedField);
-
-//       // await client.connect();
-//       // // Access the MongoDB database
-//       // const database = client.db('BES3D'); 
-
-//       const TargetModel = mongoose.model(target, mongoose.Schema({}), target);
-//       const result = await TargetModel.aggregate([
-//         // Filter the documents
-//         { $match: parameters },
-//         // Select the fields to return
-//         { $project: selectedField },
-//         // Skip the first 20 documents
-//         { $skip: 0 },
-//         // Limit the result eg 20 documents (from 21 to 40)
-//         { $limit: maxRow }
-//       ]);
-
-//       const totalRecord = await TargetModel.countDocuments();
-
-//       // console.log(totalRecord);
-
-//       const responseData = result.map((document) => {
-//         const documentData = {};
-//         tableCol.forEach((field) => {
-//           documentData[field] = document[field];
-//         });
-//         return documentData;
-//       });
-//       // console.log(responseData);
-
-//       res.send({
-//         target: target,
-//         action: action,
-//         tableCol: tableCol,
-//         tableRow: responseData,
-//         display: display,
-//         totalPage: Math.ceil(totalRecord / maxRow),
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send(error);
-//   }
-// });
-
-module.exports = router;
